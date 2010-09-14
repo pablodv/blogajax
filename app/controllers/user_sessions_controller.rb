6fa84af20fbf139c0c6877a2f929a19495d6b161
insignia4u <insignia4u@insignia4u-desktop.(none)>
2010-09-14 15:56:11 -0300

    Version Final

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,7 +11,15 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_to posts_path #redirect_back_or_default admin_clients_path
+      unless request.xhr?
+        redirect_back_or_default posts_path
+      else
+#        render :update do |page|
+#          page.replace_html 'posts', :partial => 'comment'
+#          page[:user_sessions_new].clear
+#          page["comment_#commentsp"].visual_effect :highlight, :duration => 3.5
+#        end
+      end
     else
       render :action => :new
     end
@@ -20,7 +28,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default login_path
+    redirect_to posts_path
   end
   
 end
